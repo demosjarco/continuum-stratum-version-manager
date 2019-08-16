00e281275e17266e032b231f3a56127d233c7661
demosjarco <demosjarco@users.noreply.github.com>
2019-08-16 16:34:16 -0700

    Download stratum

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ function checkIfPATavailable() {
 	console.log('Checking if personal access token for gitlab is set');
 	if (process.env.GITLAB_PAT) {
 		console.log(successColor('got it'));
+		getListOfSizes();
 	} else {
 		console.log(errorColor('none found'));
 		fs.access('./.env', fs.constants.W_OK, (err1) => {
 			if (err1) {
 				if (err1.code === 'ENOENT') {
-					// Create file
 					getUserPAT(true);
 				} else {
 					console.error(errorColor('is read-only'));
@@ -67,13 +67,12 @@ function getUserPAT(createFile = false) {
 		output: process.stdout
 	});
 
-	console.log('To get higher res (256x, 512x, 1024x, 2048x) textures, you ' + terminalLink('donate', 'https://continuum.graphics/stratum-rp') + ". Once you do, you must create a personal access token with the scope 'api' for this program to connect. You can do so under " + terminalLink('User Settings > Access Tokens', 'https://dl.continuum.graphics/profile/personal_access_tokens'));
+	console.log('To get higher res (256x, 512x, 1024x, 2048x) textures, you have to ' + terminalLink('donate', 'https://continuum.graphics/stratum-rp') + ". Once you do, you must create a personal access token with the scope 'api' for this program to connect. You can do so under " + terminalLink('User Settings > Access Tokens', 'https://dl.continuum.graphics/profile/personal_access_tokens'));
 	console.log("If you'd prefer the free 128x version, simply leave the following blank");
-	r1.question('Personal Access Token:', (answer) => {
+	r1.question('Personal Access Token: ', (answer) => {
 		processPat(answer.trim());
 
 		r1.close();
-		process.stdin.destroy();
 	});
 
 	function processPat(pat) {
@@ -85,6 +84,7 @@ function getUserPAT(createFile = false) {
 
 			fs.writeFile('./.env', newEnvFile, (err2) => {
 				if (err2) throw err2;
+				require('dotenv').config();
 
 				getListOfSizes();
 			});
@@ -93,6 +93,7 @@ function getUserPAT(createFile = false) {
 }
 
 const request = require('request');
+let stratumVersions = [];
 
 function getListOfSizes() {
 	request.get({
@@ -103,13 +104,103 @@ function getListOfSizes() {
 	}, function (error, response, body) {
 		if (error) throw error;
 
-		let stratumVersions = [];
 		JSON.parse(body).map(function (project) {
 			const validProjectNames = /^Stratum\-\d+x$/;
 			if (validProjectNames.test(project.name)) {
 				stratumVersions.push(project);
 			}
 		});
+
+		askSizes();
 	});
 
+}
+
+function askSizes() {
+	const r1 = rl.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+	console.log('Select a version to download. If left blank, it will download the highest res version');
+	let question = '';
+	stratumVersions.map(function (versions, index) {
+		question += '[' + index + '] ' + versions.name + '\n';
+	});
+	question += 'Type the corresponding number: ';
+	r1.question(question, (answer) => {
+		const version = Number.isInteger(answer) ? Number(answer) : stratumVersions.length - 1;
+		console.log(successColor('Installing ' + stratumVersions[version].name));
+		installStratum(version, stratumVersions[version].name);
+
+		r1.close();
+	});
+	r1.once('close', () => {
+		process.stdin.destroy();
+	});
+}
+
+function installStratum(version, name) {
+	console.log('Checking for old stratum to remove');
+
+	fs.readdir(texturePackDirectory, function (err, files) {
+		if (err) throw err;
+
+		const oldFolderReg = /^Stratum\-\d+x\-master(\-(\w|\d)+)?$/i;
+		let exists = false;
+		files.forEach(function (fileName) {
+			if (oldFolderReg.test(fileName)) {
+				exists = true;
+				console.log('Deleting old stratum version');
+				require('rimraf')(texturePackDirectory + '/' + fileName + '/', function (err2) {
+					if (err2) throw err2;
+
+					console.log(successColor('Deleted old stratum version'));
+					downloadStratum('https://dl.continuum.graphics/api/v4/projects/' + stratumVersions[version].id + '/repository/archive.zip');
+				});
+			}
+		});
+		if (!exists) {
+			console.log(successColor('No old version detected'));
+			downloadStratum('https://dl.continuum.graphics/api/v4/projects/' + stratumVersions[version].id + '/repository/archive.zip');
+		}
+	});
+
+	function downloadStratum(link) {
+		console.log('Downloading stratum zip');
+		const fileNameReg = /(?<=htt(p:|ps:)\/\/dl\.continuum\.graphics\/api\/v4\/projects\/\d+\/repository\/)\w+(?=\.zip)/;
+		const newFileName = name + '-master.zip';
+		let fileSize = 0;
+		let downloadedSize = 0;
+		const ProgressBar = require("./ProgressBar");
+		const Bar = new ProgressBar();
+
+		if (fileNameReg.test(link)) {
+			request({
+				url: link.replace(/http(?!s)/g, "https"),
+				headers: {
+					'Private-Token': process.env.GITLAB_PAT || ''
+				}
+			}).on('response', function (response) {
+				fileSize = Number(response.headers['content-length']);
+				Bar.init(fileSize);
+			}).once("error", (error) => {
+				console.error(error)
+			}).on('data', function (chunk) {
+				downloadedSize += Number(chunk.length);
+				Bar.update(downloadedSize);
+			}).once('close', function () {
+				console.log(successColor('Downloaded stratum archive'));
+			}).pipe(fs.createWriteStream(texturePackDirectory + '/' + newFileName).once('close', () => {
+				const AdmZip = require('adm-zip');
+				let zip = new AdmZip(texturePackDirectory + '/' + newFileName);
+				zip.extractAllTo(texturePackDirectory + '/', true);
+				console.log('Extracted stratum archive');
+				fs.unlink(texturePackDirectory + '/' + newFileName, (err) => {
+					if (err) throw err;
+					console.log(successColor('Deleted stratum archive'));
+				});
+			}));
+		}
+	}
 }
